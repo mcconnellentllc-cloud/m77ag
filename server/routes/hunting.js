b2f9381efd13d76fa60a50967c278ec8cebb3ff2
M77 Ag <mcconnellentllc@gmail.com>
2025-11-22 20:53:09 -0700

    Update hunting.js

--- server/routes/hunting.js
+++ server/routes/hunting.js
@@ -1,17 +1,15 @@
-// routes/hunting.js
 const express = require('express');
 const router = express.Router();
-const huntingController = require('../controllers/huntingController');
+const bookingController = require('../controllers/bookingController');
 const { authenticate, isAdmin } = require('../middleware/auth');
 
 // Public routes
-router.post('/bookings', huntingController.createBooking);
-router.post('/submit-waiver', huntingController.submitWaiver);
-router.post('/update-payment', huntingController.updatePayment);
-router.get('/bookings/:id', huntingController.getBookingById);
+router.post('/bookings', bookingController.createBooking);
+router.post('/submit-waiver', bookingController.submitWaiver);
+router.get('/bookings/:id', bookingController.getBookingById);
 
 // Admin routes (protected)
-router.get('/admin/bookings', authenticate, isAdmin, huntingController.getAllBookings);
-router.delete('/admin/bookings/:id', authenticate, isAdmin, huntingController.cancelBooking);
+router.get('/admin/bookings', authenticate, isAdmin, bookingController.getAllBookings);
+router.delete('/admin/bookings/:id', authenticate, isAdmin, bookingController.cancelBooking);
 
 module.exports = router;
